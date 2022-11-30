eef6edb80a4331db4a39e70fe107e1bfe9c16d95
subAngel <whoangel.agl@gmail.com>
2022-11-30 08:09:34 -0600

    upload

---
+++ pdf.js
@@ -0,0 +1,15 @@
+const pdf = require("html-pdf");
+const fs = require("fs");
+
+const content = `
+<h1>Título en el PDF creado con el paquete html-pdf</h1>
+<p>Generando un PDF con un HTML sencillo</p>
+`;
+
+pdf.create(content).toFile("./html-pdf.pdf", function (err, res) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(res);
+	}
+});
